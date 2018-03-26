5168c9e2bfd7ce15e1a755bfc745012facdc107a
Jesper Sand Nielsen <Sansemand@users.noreply.github.com>
2018-03-26 10:20:19 +0200

    Update Parser.scala

--- main/src/ca/uwaterloo/flix/language/phase/Parser.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Parser.scala
@@ -606,7 +606,7 @@ class Parser(val source: Source) extends org.parboiled2.Parser {
     def Primary: Rule1[ParsedAst.Expression] = rule {
       LetRec | LetMatch | IfThenElse | Match | LambdaMatch | SelectChannel | Switch | Unsafe | Native | Lambda | Tuple |
         ArrayLit | ArrayNew | FNil | FSet | FMap | Literal |
-        HandleWith | Existential | Universal | UnaryLambda | QName | Wild | Tag | SName | Hole | UserError /*| Spawn*/
+        HandleWith | Existential | Universal | UnaryLambda | QName | Wild | Tag | SName | Hole | UserError | Spawn
     }
 
     def Literal: Rule1[ParsedAst.Expression.Lit] = rule {
