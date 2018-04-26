d07cd510f271333ecbf8a2045405a57e07ab29c5
Jesper Sand Nielsen <jesper.sand.nielsen@gmail.com>
2018-04-26 11:55:15 +0200

    initialize channel methods

--- main/src/ca/uwaterloo/flix/runtime/interpreter/Value.scala
+++ main/src/ca/uwaterloo/flix/runtime/interpreter/Value.scala
@@ -21,6 +21,8 @@ import ca.uwaterloo.flix.language.ast.{Symbol, Type}
 import ca.uwaterloo.flix.util.InternalRuntimeException
 import java.util.concurrent.ConcurrentLinkedQueue
 
+import jdk.jshell.spi.ExecutionControl.NotImplementedException
+
 sealed trait Value
 
 object Value {
@@ -202,7 +204,7 @@ object Value {
       }
     }
 
-    def put(): AnyRef = throw InternalRuntimeException("Channel.put is not implemented")
+    def put(): AnyRef = throw new NotImplementedException("Channel.put is not implemented")
 
     def notifyGet(): Unit = waitingGetters.asInstanceOf[ConcurrentLinkedQueue[Thread]].peek().notify()
 
